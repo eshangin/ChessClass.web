23bac96a97a606c4fa0728b950827d322b3934f1
eshangin <shangin.jekon@mail.ru>
2018-11-22 16:36:38 +0700

    chess move validation with chess.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import * as ChessBoard from 'chessboardjs';
+import * as Chess from 'chess.js';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,62 @@ import * as ChessBoard from 'chessboardjs';
 })
 export class AppComponent implements OnInit {
 
-  board: ChessBoardInstance;
+  private engine: ChessInstance;
+  private board: ChessBoardInstance;
+  //currentFen: string = 'r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - c3 0 19';
+  currentFen: string = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+  currentPgn: string;
   title = 'Hi!';
 
+  constructor() {
+    this.engine = new Chess();
+  }  
+
   ngOnInit(): void {
+    this.engine.load(this.currentFen);
+
     const boardConfig: ChessBoardJS.BoardConfig = {
       draggable: true,
       pieceTheme: '/images/chesspieces/{piece}.png',
-      position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
+      onDragStart: (piece) => this.onDragStart(piece),
+      onDrop: (source, target) => this.onDrop(source, target),
+      onSnapEnd: () => this.onSnapEndFunc()
     };
 
-    const board = ChessBoard('board', boardConfig);
+    this.board = ChessBoard('board', boardConfig);
+    this.board.position(this.engine.fen());
+  }
+
+  private onDrop(source: string, target: string): string {
+    const tryMove: ChessJS.Move = {
+        from: source,
+        to: target,
+        promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    };
+    const move = this.engine.move(tryMove);
+
+    if (move == null) {
+        return 'snapback';
+    }
+  };
+
+  private onSnapEndFunc() {
+    return this.board.position(this.engine.fen());
+  }
+
+  // do not pick up pieces if the game is over
+  // only pick up pieces for the side to move
+  private onDragStart(piece: string): boolean {
+    return (
+        !this.engine.game_over() &&
+        this.allowMove(this.engine.turn(), piece)
+    );
+  };
+
+  private allowMove(turn: string, piece: string): boolean {
+    return !(
+        ((String(turn).toLowerCase() === 'w') && (piece.search(/^b/) !== -1)) ||
+        ((String(turn).toLowerCase() === 'b') && (piece.search(/^w/) !== -1))
+    );
   }
 }
