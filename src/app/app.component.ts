703bdbb0afee0802b6e408b07f7851ad4cdc8d3a
eshangin <shangin.jekon@mail.ru>
2018-11-22 13:29:23 +0700

    view chess board using chessboardjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as ChessBoard from 'chessboardjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  board: ChessBoardInstance;
   title = 'Hi!';
+
+  ngOnInit(): void {
+    const boardConfig: ChessBoardJS.BoardConfig = {
+      draggable: true,
+      pieceTheme: 'https://s3-us-west-2.amazonaws.com/chessimg/{piece}.png',
+      position: 'start'
+    };
+
+    const board = ChessBoard('board', boardConfig);
+  }
 }
