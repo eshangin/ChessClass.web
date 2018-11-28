2f1a02863cedbcdadcd10c61a14203a9b00706d4
eshangin <shangin.jekon@mail.ru>
2018-11-28 19:20:44 +0700

    cleanup

--- src/app/services/fake-backend.interceptor.ts
+++ src/app/services/fake-backend.interceptor.ts
@@ -31,14 +31,6 @@ export class FakeBackendInterceptor implements HttpInterceptor {
 
         const db = this.getDb();
 
-        // const pupils = [
-        //     { id: 'a', name: 'Иван З', homeworks: [{id: 1, puzzles: [ puzzles[0], puzzles[1] ] }] },
-        //     { id: 'b', name: 'Сергей Б' },
-        //     { id: 'c', name: 'Анна К' },
-        //     { id: 'd', name: 'Игорь П' },
-        //     { id: 'e', name: 'Сергей Н' }
-        // ];
-
         let result = null;
 
         if (request.url.match(/api\/puzzles/) && request.method === 'GET') {
@@ -80,119 +72,6 @@ export class FakeBackendInterceptor implements HttpInterceptor {
 
         // pass through any requests not handled above
         return next.handle(request);
-
-        /*
-        // authenticate
-        if (request.url.endsWith('/api/authenticate') && request.method === 'POST') {
-            // find if any user matches login credentials
-            let filteredUsers = users.filter(user => {
-                return user.username === request.body.username && user.password === request.body.password;
-            });
-
-            if (filteredUsers.length) {
-                // if login details are valid return 200 OK with user details and fake jwt token
-                let user = filteredUsers[0];
-                let body = {
-                    id: user.id,
-                    username: user.username,
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                    token: 'fake-jwt-token'
-                };
-
-                return of(new HttpResponse({ status: 200, body: body }));
-            } else {
-                // else return 400 bad request
-                return Observable.throw('Username or password is incorrect');
-            }
-        }
-
-        // get users
-        if (request.url.endsWith('/api/users') && request.method === 'GET') {
-            // check for fake auth token in header and return users if valid, this security is implemented server side in a real application
-            if (request.headers.get('Authorization') === 'Bearer fake-jwt-token') {
-                return of(new HttpResponse({ status: 200, body: users }));
-            } else {
-                // return 401 not authorised if token is null or invalid
-                return Observable.throw('Unauthorised');
-            }
-        }
-
-        // get user by id
-        if (request.url.match(/\/api\/users\/\d+$/) && request.method === 'GET') {
-            // check for fake auth token in header and return user if valid, this security is implemented server side in a real application
-            if (request.headers.get('Authorization') === 'Bearer fake-jwt-token') {
-                // find user by id in users array
-                let urlParts = request.url.split('/');
-                let id = parseInt(urlParts[urlParts.length - 1]);
-                let matchedUsers = users.filter(user => { return user.id === id; });
-                let user = matchedUsers.length ? matchedUsers[0] : null;
-
-                return of(new HttpResponse({ status: 200, body: user }));
-            } else {
-                // return 401 not authorised if token is null or invalid
-                return Observable.throw('Unauthorised');
-            }
-        }
-
-        // create user
-        if (request.url.endsWith('/api/users') && request.method === 'POST') {
-            // get new user object from post body
-            let newUser = request.body;
-
-            // validation
-            let duplicateUser = users.filter(user => { return user.username === newUser.username; }).length;
-            if (duplicateUser) {
-                return Observable.throw('Username "' + newUser.username + '" is already taken');
-            }
-
-            // save new user
-            newUser.id = users.length + 1;
-            users.push(newUser);
-            localStorage.setItem('users', JSON.stringify(users));
-
-            // respond 200 OK
-            return of(new HttpResponse({ status: 200 }));
-        }
-
-        // delete user
-        if (request.url.match(/\/api\/users\/\d+$/) && request.method === 'DELETE') {
-            // check for fake auth token in header and return user if valid, this security is implemented server side in a real application
-            if (request.headers.get('Authorization') === 'Bearer fake-jwt-token') {
-                // find user by id in users array
-                let urlParts = request.url.split('/');
-                let id = parseInt(urlParts[urlParts.length - 1]);
-                for (let i = 0; i < users.length; i++) {
-                    let user = users[i];
-                    if (user.id === id) {
-                        // delete user
-                        users.splice(i, 1);
-                        localStorage.setItem('users', JSON.stringify(users));
-                        break;
-                    }
-                }
-
-                // respond 200 OK
-                return of(new HttpResponse({ status: 200 }));
-            } else {
-                // return 401 not authorised if token is null or invalid
-                return Observable.throw('Unauthorised');
-            }
-        }
-
-        // pass through any requests not handled above
-        return next.handle(request);
-        */
-
-        /*
-        // wrap in delayed observable to simulate server api call
-        return mergeMap(() => {
-        });
-        // call materialize and dematerialize to ensure delay even if an error is thrown (https://github.com/Reactive-Extensions/RxJS/issues/648)
-        .materialize()
-        .delay(500)
-        .dematerialize();
-        */
     }
 
     private updateStoragePuzzles(puzzles: Puzzle[]) {
