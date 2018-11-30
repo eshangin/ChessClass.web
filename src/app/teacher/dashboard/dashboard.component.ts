780d1ded3c4945a3f9d5e58e7f0cf604a39f3990
eshangin <shangin.jekon@mail.ru>
2018-11-30 15:57:38 +0700

    create class: add some UI animations

--- src/app/teacher/dashboard/dashboard.component.ts
+++ src/app/teacher/dashboard/dashboard.component.ts
@@ -4,28 +4,48 @@ import {SchoolClassService} from 'src/app/services/school-class.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {CreateClassModalComponent} from '../create-class-modal/create-class-modal.component';
 import {AddPupilToClassModalComponent} from '../add-pupil-to-class-modal/add-pupil-to-class-modal.component';
+import {trigger, transition, animate, style} from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+     trigger('anim', [
+        // transition('* => void', [
+        //   style({ height: '*', opacity: '1', transform: 'translateX(0)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'}),
+        //   sequence([
+        //     animate(".25s ease", style({ height: '*', opacity: '.2', transform: 'translateX(20px)', 'box-shadow': 'none'  })),
+        //     animate(".1s ease", style({ height: '0', opacity: 0, transform: 'translateX(20px)', 'box-shadow': 'none'  }))
+        //   ])
+        // ]),
+        transition('void => active', [
+          style({ height: '0', opacity: '0' }),
+          animate(".50s ease", style({ height: '*', opacity: 1 }))
+        ])
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
 
-  myClasses: SchoolClass[] = [];
+  gClasses: [Array<SchoolClass>] = [Array<SchoolClass>()];
 
   constructor(
     private classService: SchoolClassService,
     private modalService: NgbModal) { }
 
   ngOnInit() {
-    this.classService.getClasses().subscribe(classes => this.myClasses = classes);
+    this.classService.getClasses().subscribe(classes => {
+      classes.forEach(c => this.addClassToGroup(c));
+    });
   }
 
   onCreateClassClick() {
     const modalRef = this.modalService.open(CreateClassModalComponent, {ariaLabelledBy: 'modal-basic-title'});
     modalRef.result.then((result) => {
-      this.myClasses.push(result.class);
+      result.class.uiState = 'active';
+      this.addClassToGroup(result.class);
+      console.log(this.gClasses);
     }, () => {});
   }
 
@@ -38,15 +58,11 @@ export class DashboardComponent implements OnInit {
     }, () => {});
   }
 
-  get gropedClasses() {
+  private addClassToGroup(c: SchoolClass) {
     const groupBy = 3;
-    return this.myClasses.reduce((p, c) => {
-      if (p[p.length - 1].length == groupBy) {
-        p.push([]);
-      }
-      let lastArr = p[p.length - 1];
-      lastArr.push(c);
-      return p;
-    }, [[]]);
+    if (this.gClasses[this.gClasses.length - 1].length == groupBy) {
+      this.gClasses.push(Array<SchoolClass>());
+    }
+    this.gClasses[this.gClasses.length - 1].push(c);
   }
 }
