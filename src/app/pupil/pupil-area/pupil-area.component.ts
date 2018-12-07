7339293a8ce12351ca02c4363aef514173a3b822
eshangin <shangin.jekon@mail.ru>
2018-12-07 11:06:38 +0700

    small fix

--- src/app/pupil/pupil-area/pupil-area.component.ts
+++ src/app/pupil/pupil-area/pupil-area.component.ts
@@ -14,7 +14,7 @@ export class PupilAreaComponent implements OnInit {
     private authService: AuthService) { }
 
   ngOnInit() {
-    this.pupilName = this.authService.currentUser.getFullName();
+    //this.pupilName = this.authService.currentUser.getFullName();
   }
 
 }
