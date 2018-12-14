c33fbb037af7fbcc953b1d17320c662c611953f2
eshangin <shangin.jekon@mail.ru>
2018-12-14 13:44:45 +0700

    some UI updates

--- src/app/pupil/pupil.module.ts
+++ src/app/pupil/pupil.module.ts
@@ -7,13 +7,15 @@ import { PupilHomeComponent } from './pupil-home/pupil-home.component';
 import {SharedModule} from '../shared/shared.module';
 import { DoHomeworkComponent } from './do-homework/do-homework.component';
 import { PupilTimelineComponent } from './pupil-timeline/pupil-timeline.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [PupilAreaComponent, PupilHomeComponent, DoHomeworkComponent, PupilTimelineComponent],
   imports: [
     CommonModule,
     PupilRoutingModule,
-    SharedModule
+    SharedModule,
+    NgbModule
   ]
 })
 export class PupilModule { }
